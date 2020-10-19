2ec80d0c74513ab826ba32c28b4173de5b83f690
djsteve33 <swomble@aol.com>
2020-10-18 21:56:05 -0400

    changed band generator to users and revised concerts generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :venues
-  resources :bands
   resources :concerts
+  resources :users
+  resources :venues
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20201019015147_create_concerts.rb
+++ db/migrate/20201019015147_create_concerts.rb
@@ -1,9 +1,10 @@
 class CreateConcerts < ActiveRecord::Migration[6.0]
   def change
     create_table :concerts do |t|
+      t.string :band_name
       t.string :date
       t.string :start_time
-      t.integer :band_id
+      t.integer :user_id
       t.integer :venue_id
 
       t.timestamps
