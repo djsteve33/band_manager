497969fb927c0166973c8faca1edc783ae1a7174
djsteve33 <swomble@aol.com>
2020-10-20 20:35:53 -0400

    added associations and validations to Concert model

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -5,45 +5,45 @@ class ConcertsController < ApplicationController
         @concerts = Concert.all
     end
 
-    def show
-    end
+    # def show
+    # end
 
-    def new
-        @concert = Concert.new
-    end
+    # def new
+    #     @concert = Concert.new
+    # end
 
-    def create
-        @concert = Concert.new(concert_params)
-    if @concert.save
-        redirect_to concert_path(@concert)
-    else
-        render :new
-        end
-    end
+    # def create
+    #     @concert = Concert.new(concert_params)
+    #     if @concert.save
+    #     redirect_to concert_path(@concert)
+    #     else
+    #     render :new
+    #     end
+    # end
 
-    def edit
-    end
+    # def edit
+    # end
 
-    def update
-        @concert.update(params.require(:concert).permit(:band_name, :date, :start_time, :user_id, :venue_id)
-        redirect_to @concert
-    end
+    # def update
+    #     @concert.update(params.require(:concert).permit(:band_name, :date, :start_time, :user_id, :venue_id)
+    #     redirect_to @concert
+    # end
 
-    def destroy
-        @concert.destroy
-        redirect_to concerts_path
-    end
+    # def destroy
+    #     @concert.destroy
+    #     redirect_to concerts_path
+    # end
 
-    private
+    # private
 
-    def concert_params
-        params.require[:concert].permit(:band_name, :date, :start_time, :user_id, :venue_id,
-        venues_attributes: [:name, :city, :state, :capacity])
-    end
+    # def concert_params
+    #     params.require[:concert].permit(:band_name, :date, :start_time, :user_id, :venue_id,
+    #     venues_attributes: [:name, :city, :state, :capacity])
+    # end
 
-    def set_concert
-        @concert = Concert.find_by_id(params[:id])
-    end
+    # def set_concert
+    #     @concert = Concert.find_by_id(params[:id])
+    # end
 
 
 
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,2 +1,8 @@
 class Concert < ApplicationRecord
+    belongs_to :user
+    belongs_to :venue
+    accepts nested_attributes_for :venue, reject_if :all_blank
+    validates :band_name, presence: true
+    vaildates :date, presence: true
+    vaildates :start_time, presence: true
 end
