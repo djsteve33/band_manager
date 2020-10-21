c285847ff3f7f26153aa5023afcc3da58b1a9668
djsteve33 <swomble@aol.com>
2020-10-21 16:21:47 -0400

    added new, create, and index methods to Venues Controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,9 +2,31 @@ class VenuesController < ApplicationController
     before_action :set_concert
     before_action :set_venue, only: [:show]
 
+    def new
+    @venue = @concert.venues.new
+    end
+
+    def create
+    @venue = current_user.@concert.venues.build(venue_params)
+        if @venue.save
+        redirect_to [@concert, @venue]
+        else
+        render :new
+        end
+    end
+
     def show
     @venue = Venue.find_by_id(params[:id])
     end
+
+    def index
+        @venuees = if params[:concert_id]
+            Venue.find_by_concert_id(params[:concert_id].alpha_order
+        else
+            current_user.@concert.venues
+        end
+    end
+            
 
     private
 
