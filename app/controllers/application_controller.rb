803f7dcb3b18bd9fc0fb518031082437d8c5c1ec
djsteve33 <swomble@aol.com>
2020-10-20 21:14:29 -0400

    added assocations to Venue model, created a concerts partial file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    helper_methods :current_user
+
+    private
+
+    def current_user
+    @user ||= User.find_by_id(session[:user_id])
+    end
+
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,9 @@
 class Venue < ApplicationRecord
+    has_many :concerts
+    has_many :users, through: :concerts
+
+    def self.find_by_user_id(id)
+    where(user_id: id)
+    end
+
 end
