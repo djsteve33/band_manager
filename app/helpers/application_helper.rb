a3ecf51dafab4c73585f8ca43753f337dd06cf0e
djsteve33 <swomble@aol.com>
2020-10-22 16:17:10 -0400

    added nested form to Concerts new view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
         if session[:user_id]
         link_to “Logout”, ‘/logout’, method: ‘delete’
         else
-        link_to(‘Login”, ‘/login’)  +  ‘ ‘ +  link_to(‘sign up’, ‘/signup’)
+        link_to(‘Login', ‘/login’)  +  ‘ ‘ +  link_to(‘Sign Up’, ‘/signup’)
         end
     end
 
