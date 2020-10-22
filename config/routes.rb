a8c922162c14a076938c2fc79d8b29b41e32a7ac
djsteve33 <swomble@aol.com>
2020-10-22 16:40:22 -0400

    added routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
-  get '/login', to: 'sesdsions#new'
+  get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
@@ -9,8 +9,14 @@ Rails.application.routes.draw do
   resources :venues, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/concerts', to: 'concerts#index'
+  post '/concerts', to: 'concerts#create'
   get '/concert/new', to: 'concerts#new'
-  get '/show', to: 'concerts#show'
+  get '/concerts/show', to: 'concerts#show'
+  
+  get '/sessions/new', to: 'sessions#new'
+
+  get '/venue/new', to: 'venues#new'
 
   resources :concerts do
     resources :venues
