26f55135f4e8228f037e55c59b7eebc7f8e8bf5c
djsteve33 <swomble@aol.com>
2021-02-13 17:59:05 -0500

    added/updated validations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
     has_secure_password
     has_many :concerts
     has_many :venues, through: :concerts
+
     validates :username, :email, presence: true
-    validates :username, :email, uniqueness: true
+    validates :email, uniqueness: true
+    validates :password, length: { in: 5..10 }
 
     def self.create_from_omniauth(auth)
         User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
