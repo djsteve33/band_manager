70ed2ec6c11022fbbdc2984526c82ebfd849809f
djsteve33 <swomble@aol.com>
2021-02-17 16:02:28 -0500

    added methods to Venues Controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,6 @@
 class VenuesController < ApplicationController
 #     before_action :set_concert, only: [:show, :update, :edit, :destroy]
-#     before_action :set_venue, only: [:show, :update, :edit, :destroy]
+      before_action :set_venue, only: [:show, :update, :edit, :destroy]
       before_action :redirect_if_not_logged_in
 
       def new
@@ -20,33 +20,55 @@ class VenuesController < ApplicationController
       end     
 
 
-#     def create
-#     @venue = current_user.@concert.venues.build(venue_params)
-#         if @venue.save
-#         redirect_to [@concert, @venue]
-#         else
-#         render :new
-#         end
-#     end
+    def create
+    @venue = current_user.venues.build(venue_params)
+        if @venue.save
+        redirect_to venues_path
+        else
+        render :new
+        end
+    end
 
-#     def show
-#     @venue = Venue.find_by_id(params[:id])
-#     end
+    def edit
+      @venue = Venue.find_by_id(params[:id])
+      redirect_to venues_path if !@venue || @venue.user !=current_user
+    end
 
-#    
+    def update
+      @venue = Venue.find_by(id: params[:id])
+      redirect_to venues_path if !@venue || @venue.user !=current_user
+      if @venue.update(venue_params)
+            redirect_to venue_path(@venue)
+      else
+            render :edit
+      end
+    end
 
-#     private
+    
+      def show
+      @venue = Venue.find_by_id(params[:id])
+      redirect_to venues_path if !@venue
+      end
 
-#     def venue_params
-#     params.require(:venue).permit(:name, :city, :state, :capacity)
-#     end
+      def destroy
+      @venue.destroy
+      redirect_to venues_path
+      end
+
+
+
+    private
+
+    def venue_params
+    params.require(:venue).permit(:name, :city, :state, :capacity)
+    end
 
 #     def set_concert
 #     @concert= Venue.find_by_id(params[:id])
 #     end
 
-#     def set_venue
-#     @venue = .find_by_id(params[:id])
-#     end
+    def set_venue
+    @venue = .find_by(id: params[:id])
+    end
 
 end
