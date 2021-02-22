3538240d89d1f0f8fcdfd2e090fdcbf28e3a9bbe
djsteve33 <swomble@aol.com>
2021-02-22 11:49:07 -0500

    added new method to Concerts model, updated Concerts new and edit views

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,9 +1,13 @@
 class Concert < ApplicationRecord
     belongs_to :user
     belongs_to :venue
-#     #accepts_nested_attributes_for :venues, reject_if :all_blank
+    accepts_nested_attributes_for :venue, reject_if :all_blank
     validates :band_name, :date, :start_time, presence: true
-    
-    scope :order_by_date, -> { order(date: 'asc')}
+    scope :order_by_date, -> { order(date: :asc) }
+
+    def venue_and_band_name
+        "#{self.venue.name} - #{self.band_name}"
+    end
+
 
 end
