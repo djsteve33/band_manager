a9ee71b5a4bf04a3cf776bcf7e03df87ec7ce11d
djsteve33 <swomble@aol.com>
2021-02-28 18:10:07 -0500

    added venue attributes method to Concert.rb

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,9 +1,15 @@
 class Concert < ApplicationRecord
     belongs_to :user
     belongs_to :venue
-    accepts_nested_attributes_for :venue
+    #accepts_nested_attributes_for :venue
     validates :band_name, :date, :start_time, presence: true
     scope :order_by_date, -> { order(date: :asc) }
+
+    def venue_attributes=(attr)
+        if !attr[:name].blank?
+            self.venue = Venue.find_or_create_by(name: attr[:name], city: attr[:city], state: attr[:state], capacity: attr[:capacity])
+        end
+    end
 
     def concert_info
         "#{self.date} -  #{self.venue.name} - #{self.band_name} - #{self.start_time}"
